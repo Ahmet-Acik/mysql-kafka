c5bc6fa99a594c3853d9901fbdc7c092370ebac3
drahmetacik <a.acik@icloud.com>
2025-04-19 12:50:11 +0100

    Add entities Employee

---
+++ src/main/java/org/ahmet/mysqlkafka/model/Employee.java
@@ -0,0 +1,22 @@
+package org.ahmet.mysqlkafka.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "employees")
+@Data
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String department;
+}
