e0b8a0571bb00feaaa96be23838d605be9823d65
drahmetacik <a.acik@icloud.com>
2025-04-19 13:15:35 +0100

    Add EmployeeService to manage employee operations

---
+++ src/main/java/org/ahmet/mysqlkafka/service/EmployeeService.java
@@ -0,0 +1,24 @@
+package org.ahmet.mysqlkafka.service;
+
+import org.ahmet.mysqlkafka.model.Employee;
+import org.ahmet.mysqlkafka.repository.EmployeeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee saveEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+}
