5f85b05df17f6ec90e9d24136adbf0ec1ed70998
drahmetacik <a.acik@icloud.com>
2025-04-19 13:17:59 +0100

    Add KafkaProducerService to handle message sending

---
+++ src/main/java/org/ahmet/mysqlkafka/controller/EmployeeController.java
@@ -0,0 +1,27 @@
+package org.ahmet.mysqlkafka.controller;
+
+import org.ahmet.mysqlkafka.model.Employee;
+import org.ahmet.mysqlkafka.service.EmployeeService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+
+    @PostMapping
+    public Employee saveEmployee(@RequestBody Employee employee) {
+        return employeeService.saveEmployee(employee);
+    }
+}
