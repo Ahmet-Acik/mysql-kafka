c0b7eaaae820626cc9cddd27e5c5276fef79dc15
drahmetacik <a.acik@icloud.com>
2025-04-19 13:23:36 +0100

    Add ProjectService for managing project data and repository interactions

---
+++ src/main/java/org/ahmet/mysqlkafka/service/ProjectService.java
@@ -0,0 +1,24 @@
+package org.ahmet.mysqlkafka.service;
+
+import org.ahmet.mysqlkafka.model.Project;
+import org.ahmet.mysqlkafka.repository.ProjectRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProjectService {
+    private final ProjectRepository projectRepository;
+
+    public ProjectService(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+
+    public List<Project> getAllProjects() {
+        return projectRepository.findAll();
+    }
+
+    public Project saveProject(Project project) {
+        return projectRepository.save(project);
+    }
+}
