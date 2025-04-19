f5b3bc06074f41e2b92bbb5395b9b3ae538c4e3e
drahmetacik <a.acik@icloud.com>
2025-04-19 12:51:06 +0100

    Add  entity  EmployeeProject

---
+++ src/main/java/org/ahmet/mysqlkafka/model/EmployeeProject.java
@@ -0,0 +1,21 @@
+package org.ahmet.mysqlkafka.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "employee_projects")
+@Data
+public class EmployeeProject {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "employee_id", nullable = false)
+    private Employee employee;
+
+    @ManyToOne
+    @JoinColumn(name = "project_id", nullable = false)
+    private Project project;
+}
