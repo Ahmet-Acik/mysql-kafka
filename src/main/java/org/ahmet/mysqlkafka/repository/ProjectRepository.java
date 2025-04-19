e64024c330dd63782a4eccc32421862ee5d58d14
drahmetacik <a.acik@icloud.com>
2025-04-19 12:53:28 +0100

    Add repository interface for ProjectRepository entity

---
+++ src/main/java/org/ahmet/mysqlkafka/repository/ProjectRepository.java
@@ -0,0 +1,7 @@
+package org.ahmet.mysqlkafka.repository;
+
+import org.ahmet.mysqlkafka.model.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+}
