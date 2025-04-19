45d96cf2d05740055c1db03419cb4e9959a219dd
drahmetacik <a.acik@icloud.com>
2025-04-19 13:23:12 +0100

    Add DepartmentService for managing department data and repository interactions

---
+++ src/main/java/org/ahmet/mysqlkafka/service/DepartmentService.java
@@ -0,0 +1,24 @@
+package org.ahmet.mysqlkafka.service;
+
+import org.ahmet.mysqlkafka.model.Department;
+import org.ahmet.mysqlkafka.repository.DepartmentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DepartmentService {
+    private final DepartmentRepository departmentRepository;
+
+    public DepartmentService(DepartmentRepository departmentRepository) {
+        this.departmentRepository = departmentRepository;
+    }
+
+    public List<Department> getAllDepartments() {
+        return departmentRepository.findAll();
+    }
+
+    public Department saveDepartment(Department department) {
+        return departmentRepository.save(department);
+    }
+}
